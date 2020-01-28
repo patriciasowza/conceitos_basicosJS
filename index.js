3330a20ea2716caf975ed1ac50a016ebe073d6cf
Patrícia Sowza <patriciasowzabarros@gmail.com>
2020-01-27 21:45:50 -0300

    CRUD JS

---
+++ index.js
@@ -0,0 +1,67 @@
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+const users = ['Patrícia','Arthur','Agatha', 'Sara'];
+
+server.use((req, res, next) => {
+  console.time('Request');
+  console.log(`Método: ${req.method}; URL: ${req.url}`);
+
+  next();
+  console.timeEnd('Request')
+});
+
+function checkUserExists(req, res, next) {
+  if (!req.body.name) {
+    return res.status(400).json({ error: 'Usuário não encontrado' })
+  }
+
+  return next();
+}
+
+function checkInArray(req, res, next) {
+  const user = users[req.params.index];
+  if (!user) {
+    return res.status(400).json({ error: 'Usuário não existe' });
+  }
+
+  req.user = user;
+
+  return next();
+}
+
+server.get('/users', (req, res) => {
+  return res.json(users);
+});
+
+server.get('/users/:index', checkInArray, (req, res) => {
+
+  return res.json(req.user);
+});
+
+server.post('/users', checkUserExists, (req, res) => {
+  const { name } = req.body;
+
+  users.push(name);
+
+  return res.json(users);
+});
+
+server.put('/users/:index', checkUserExists, checkInArray, (req, res) => {
+  const { index } = req.params;
+  const { name } = req.body;
+
+  users[index] = name;
+  return res.json(users);
+});
+
+server.delete('/users/:index', checkInArray, (req, res) => {
+  const { index } = req.params;
+  users.splice(index, 1);
+
+  return res.send();
+});
+server.listen(3000);
